1d3c3bb78c21d4f65fb95ff951959ab6dcdf5908
sreekanth b <sreekanthb@sreekanths-mac.local>
2016-11-17 23:28:29 -0600

    done

--- Bandaru_MonkeysAndBananas/ViewController.swift
+++ Bandaru_MonkeysAndBananas/ViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class ViewController: UIViewController {
  var startPoint:CGPoint!
     var numberOfBananas:Int = 0
+    var bananasmoved:Int = 0
     @IBOutlet weak var monkeyIV: UIImageView!
- 
     @IBOutlet weak var banana1_IV: UIImageView!
     @IBOutlet weak var banana2_IV: UIImageView!
     @IBOutlet weak var banana3_IV: UIImageView!
@@ -30,18 +30,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+//Creates a new problem when button is clicked
     @IBAction func newProblem(sender: AnyObject) {
         numberOfBananas = Int(arc4random_uniform(5))
         questionLabel.text = "I Want \(numberOfBananas) Bananas"
     }
+    //Checks if required bananas are moved on the monkey image.
     @IBAction func checkResult(sender: AnyObject) {
-        print(monkeyIV.center)
-        print(banana1_IV.center)
-        print(banana2_IV.center)
-        print(banana3_IV.center)
-        print(banana4_IV.center)
+        if sqrt(pow(monkeyIV.center.x-banana1_IV.center.x, 2) + pow(monkeyIV.center.y-banana1_IV.center.y, 2)) <= 80 {
+            bananasmoved += 1
+        }
+        if sqrt(pow(monkeyIV.center.x-banana2_IV.center.x, 2) + pow(monkeyIV.center.y-banana2_IV.center.y, 2)) <= 80  {
+            bananasmoved += 1
+        }
+        if sqrt(pow(monkeyIV.center.x-banana3_IV.center.x, 2) + pow(monkeyIV.center.y-banana3_IV.center.y, 2)) <= 80 {
+            bananasmoved += 1
+        }
+        if sqrt(pow(monkeyIV.center.x-banana4_IV.center.x, 2) + pow(monkeyIV.center.y-banana4_IV.center.y, 2)) <= 80  {
+             bananasmoved += 1
+        }
+        if numberOfBananas == bananasmoved{
+            self.questionLabel.text = "Thanks :)"
+        }
+        else{
+            self.questionLabel.text = "Try Again, I want \(numberOfBananas) Bananas"
+        }
+       
+        bananasmoved = 0
     }
+    
     @IBAction func handlePan(panGR:UIPanGestureRecognizer) {
         let translation = panGR.translationInView(self.view)
         if let view = panGR.view {
